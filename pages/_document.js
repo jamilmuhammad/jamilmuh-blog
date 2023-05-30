b04f33cd22379db3f83ef33245dcc55d7ee3fc4c
jamilmuhammad <mjamil1339@gmail.com>
2023-05-30 09:07:59 +0700

    update link assets integrity

--- pages/_document.js
+++ pages/_document.js
@@ -108,7 +108,12 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css" integrity="sha256-hFAmQt2dGk6G99nu2cEWz8/1JGRnBP/BsGoZwQ7WeLE=" crossorigin="anonymous" />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
+            integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
+            crossOrigin="anonymous"
+          />
           <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
           <meta
             name="google-site-verification"
