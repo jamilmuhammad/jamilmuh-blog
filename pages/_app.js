e10890adfd4e1e1e5755a516fbf8b7b3c2931982
jamilmuhammad <mjamil1339@gmail.com>
2023-05-31 06:24:42 +0700

    update import assets

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,22 @@
-import '@/css/tailwind.css'
-import '@/css/prism.css'
-import '@/css/extra.css'
-import 'katex/dist/katex.css'
-import '@fontsource/inter/variable-full.css'
-import { ThemeProvider } from 'next-themes'
-import Head from 'next/head'
-import Router from 'next/router'
-import NProgress from 'nprogress'
-import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
-import LayoutWrapper from '@/components/LayoutWrapper'
-import { ClientReload } from '@/components/ClientReload'
-import ProgressBar from 'react-scroll-progress-bar'
-import ScrollTop from '@/components/ScrollTop'
-import { SessionProvider } from 'next-auth/react'
-import { Provider } from '@lyket/react'
+import "@/css/tailwind.css"
+import "@/css/prism.css"
+import "@/css/extra.css"
+import "katex/dist/katex.css"
+import "@fontsource/inter"
+import { ThemeProvider } from "next-themes"
+import Head from "next/head"
+import Router from "next/router"
+import NProgress from "nprogress"
+import siteMetadata from "@/data/siteMetadata"
+import Analytics from "@/components/analytics"
+import LayoutWrapper from "@/components/LayoutWrapper"
+import { ClientReload } from "@/components/ClientReload"
+import ProgressBar from "react-scroll-progress-bar"
+import ScrollTop from "@/components/ScrollTop"
+import { SessionProvider } from "next-auth/react"
+import { Provider } from "@lyket/react"
 
-const isDevelopment = process.env.NODE_ENV === 'development'
+const isDevelopment = process.env.NODE_ENV === "development"
 const isSocket = process.env.SOCKET
 
 NProgress.configure({ showSpinner: false })
@@ -38,15 +38,15 @@ Router.onRouteChangeError = () => {
 
 const defaultTheme = {
   colors: {
-    primary: '#71717a',
-    secondary: '#ff00c3',
-    text: '#fff',
-    highlight: '#ff00c3',
-    icon: '#fff',
-    background: 'transparent',
+    primary: "#71717a",
+    secondary: "#ff00c3",
+    text: "#fff",
+    highlight: "#ff00c3",
+    icon: "#fff",
+    background: "transparent",
   },
   fonts: {
-    body: 'inherit',
+    body: "inherit",
   },
 }
 
