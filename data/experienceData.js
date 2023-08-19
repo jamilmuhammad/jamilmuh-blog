f4d048225630e3c6c6c45f972732d51ec442bc5e
jamilmuhammad <mjamil1339@gmail.com>
2023-08-20 03:35:07 +0700

    update dummy list experience data

--- data/experienceData.js
+++ data/experienceData.js
@@ -1,29 +1,54 @@
 const experienceData = [
   {
-    title: 'Data Engineering Intern',
-    company: 'Accenture',
-    location: 'Ahmedabad, India',
-    range: 'January 2022 - May 2022',
-    url: 'https://www.accenture.com/gr-en/about/consulting-index',
-    text1:
-      'Helped with the digital transformation of BOAT by working on their platform called ByteETL.',
-    text2:
-      'Built the encryption component for the platform using Scala and Spark which included 2 encoding functionalities : ENCRYPT and HASH.',
-    text3:
-      'Created JSON wrappers for the encryption components and automated and triggered the pipelines to run at specific intervals using CronJob.',
+    title: "Backend Engineer",
+    company: "Freelance",
+    location: "Jakarta, Indonesia",
+    range: "January 2023 - May 2023",
+    url: "https://www.wondergo.id",
+    texts: [
+      "Build a system tourism application that provide tourism content and profile with detail information such as ticketing, maps, payment, and analytics using framework express js build in nest js.",
+      "Provide integration payment service with payment gateway Finpay.",
+      "Provide authentification with JWT tokenization and OTP method.",
+      "Build a CMS based on web dashboard by core-ui template boostrap using react js.",
+      "Provide implementing react redux for state management to boost performance.",
+      "Build a landing page by next js with tailwind css and implement framer motion for animation",
+    ],
   },
   {
-    title: 'Software Engineer Analytics',
-    company: 'HOPS Healthcare',
-    location: 'Ahmedabad, India',
-    range: 'March 2021 - June 2021',
-    url: 'https://hops.healthcare/',
-    text1:
-      'Guided by Mr. Vivek Patel, to create a pipeline that extracts essential information from informal conversations.',
-    text2:
-      'Worked on creating a web app for their healthcare system for doctors to analyze and save reports.',
-    text3:
-      'Developed functions for automatically extracting important elements from reports using BioBERT and Regex.',
+    title: "Software Engineer",
+    company: "PT Lumos Inisiatif Indonesia",
+    location: "Jakarta, Indonesia",
+    range: "June 2021 - December 2022",
+    url: "https://www.lumoshive.com/",
+    texts: [
+      "Build a system HRIS that includes employment management, attendance with dynamic shift and provide online (photo) or offline (fingerprint), payroll with dynamic component modul, leaves with approval, company chart organization, inbox for internal that integrate with mail service smtp, and reporting employment feature provide export to a file (pdf/csv) using framework php build in laravel 8.",
+      "Build a system scrapping service from BNI mutation accounts for provide API service using express js. ● Build a internal company profile using framework php build in laravel 8.",
+      "Improvement a system bidding product that includes realtime bidding, authentification with email and gmail, notification with firebase cloud messaging, CMS dashboard reporting feature using framework php build in laravel 8.",
+      "Build a system CMS for portal game that provide tracker user, performance reporting, gamification, integration with Google Ads, API service for integration partner, partner management feature using framework php build in laravel 8.",
+    ],
+  },
+  {
+    title: "Software Engineer Internship",
+    company: "PT PRATAMA NUSA MANDIRI",
+    location: "Jakarta, Indonesia",
+    range: "August 2020 - May 2021",
+    url: "https://www.ptpnm.com/",
+    texts: [
+      "Build a system to provide invoice report based on CMS web dashboard using framework MVC php build in laravel 7.",
+    ],
+  },
+  {
+    title: "Backend Engineer",
+    company: "Freelance",
+    location: "Jakarta, Indonesia",
+    range: "March 2020 - August 2020",
+    url: "https://www.lumoshive.com/",
+    texts: [
+      "Provide a transaction service that includes shopping cart, transaction point, transaction history feature.",
+      "Provide integration courier service with RajaOngkir for estimate cost courier.",
+      "Provice a community service that include features QnA session, a map of members, and dynamic search for communities and members.",
+      "Build an API service for mobile application using framework MVC express js build in sequelize js.",
+    ],
   },
 ]
 
