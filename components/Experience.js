682b753b5f3a8ac438aeb200e1db465a8ffd0efd
jamilmuhammad <mjamil1339@gmail.com>
2023-08-20 03:35:52 +0700

    update layout, implement new list data

--- components/Experience.js
+++ components/Experience.js
@@ -1,11 +1,11 @@
-import Link from '@/components/Link'
+import Link from "@/components/Link"
 
-const Experience = ({ title, company, location, range, url, text1, text2, text3 }) => {
+const Experience = ({ title, company, range, url, texts }) => {
   return (
     <div className="my-3">
       <div className="flex flex-row text-xl">
-        <span className="text-gray-500 dark:text-gray-400">{title}</span>{' '}
-        <span className="text-gray-500 dark:text-gray-400">&nbsp;@&nbsp;</span>{' '}
+        <span className="text-gray-500 dark:text-gray-400">{title}</span>{" "}
+        <span className="text-gray-500 dark:text-gray-400">&nbsp;@&nbsp;</span>{" "}
         <span className="text-primary-color-500">
           <Link href={url} className="company">
             {company}
@@ -15,18 +15,15 @@ const Experience = ({ title, company, location, range, url, text1, text2, text3 
       <div>
         <div className="p-1 font-mono text-sm text-gray-400 dark:text-gray-600">{range}</div>
         <div className="p-2">
-          <div className="flex flex-row ">
-            <div className="mr-2 text-lg text-primary-color-500"> &#8227;</div>
-            <div className="text-gray-500 dark:text-gray-400">{text1}</div>
-          </div>
-          <div className="flex flex-row">
-            <div className="mr-2 text-lg text-primary-color-500"> &#8227;</div>
-            <div className="text-gray-500 dark:text-gray-400">{text2}</div>
-          </div>
-          <div className="items-top flex flex-row">
-            <div className="mr-2 text-lg text-primary-color-500"> &#8227;</div>
-            <div className="text-gray-500 dark:text-gray-400">{text3}</div>
-          </div>
+          {!texts.length && "No texts found."}
+          {texts.map((text) => {
+            return (
+              <div className="flex flex-row " key={text}>
+                <div className="mr-2 text-lg text-primary-color-500"> &#8227;</div>
+                <div className="text-gray-500 dark:text-gray-400">{text}</div>
+              </div>
+            )
+          })}
         </div>
       </div>
       <div className="justify-center text-center text-2xl font-medium text-gray-200  dark:text-gray-600">
