7d20e554291e93c03b6da2190bc120c7dfbae9a3
jamilmuhammad <mjamil1339@gmail.com>
2023-08-21 00:11:03 +0700

    update layout author

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -5,6 +5,7 @@ import Experience from "@/components/Experience"
 import experienceData from "@/data/experienceData"
 import { RoughNotation } from "react-rough-notation"
 import { AiOutlineTwitter } from "react-icons/ai"
+import siteMetadata from "@/data/siteMetadata"
 
 export default function AuthorLayout({ children, frontMatter }) {
   const {
@@ -42,8 +43,8 @@ export default function AuthorLayout({ children, frontMatter }) {
               blurDataURL="/static/images/SVG-placeholder.png"
             />
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <div className="text-gray-500 dark:text-gray-400">{siteMetadata.occupation}</div>
+            <div className="text-gray-500 dark:text-gray-400">{siteMetadata.company}</div>
             <div className="flex flex-col pt-3">
               <a
                 className="rounded-full border px-8 py-2 text-center text-sm font-light text-gray-700 transition-colors hover:border-[#1DA1F2] hover:bg-[#1DA1F2] hover:text-white hover:shadow dark:text-white"
@@ -67,44 +68,8 @@ export default function AuthorLayout({ children, frontMatter }) {
                 animationDelay={300}
                 animationDuration={3000}
               >
-                {text1} Currently, I am focused on building data pipelines and automating them at{" "}
-                <Link
-                  href={"https://www.accenture.com/in-en"}
-                  className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
-                >
-                  Accenture
-                </Link>
-              </RoughNotation>
-            </p>
-            <br />
-            <p>
-              This is what I am doing right{" "}
-              <Link
-                href={"/now"}
-                className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
-              >
-                now
-              </Link>
-            </p>
-            <br />
-            <p className="sm:block md:hidden lg:hidden">
-              I am always looking to learn new things. I am currently working on a few projects
-              related to
-              <span className="font-semibold">
-                {" "}
-                Natural Language Processing and Machine Learning.{" "}
-              </span>
-              At the same time I am{" "}
-              <RoughNotation
-                type="underline"
-                show={true}
-                color="#FBCFE8"
-                animationDelay={1500}
-                animationDuration={3000}
-                multiline={true}
-              >
-                actively on the lookout for remote internships which I can pursue in field of Data
-                Science.
+                {`Welcome to ${siteMetadata.description}. I am Backend Engineer based on Jakarta, Indonesia. I am passionate about Web/Mobile Application and System Architecture. I really enjoy travelling and adventure to fulfill imagination to write a code. `}
+                side projects and learning new technologies.
               </RoughNotation>
             </p>
             <p className="hidden md:block">
@@ -115,13 +80,11 @@ export default function AuthorLayout({ children, frontMatter }) {
                 animationDuration="3000"
                 type="highlight"
                 color="#0ea4e9"
-                strokeWidth="3"
+                strokeWidth="1"
                 show={true}
               >
-                <span className="text-black dark:text-white">
-                  Natural Language Processing and Machine Learning.{" "}
-                </span>
-              </RoughNotation>
+                <span className="text-black dark:text-white"> Software Engineer. </span>
+              </RoughNotation>{" "}
               At the same time I am{" "}
               <RoughNotation
                 type="underline"
@@ -131,11 +94,10 @@ export default function AuthorLayout({ children, frontMatter }) {
                 animationDuration={3000}
                 multiline={true}
               >
-                actively on the lookout for remote internships which I can pursue in field of Data
-                Science
+                actively on the lookout for remote opportunity which I can pursue in field of
+                Software Engineer
               </RoughNotation>
             </p>
-            <br />
             <p>
               I am a strong advocate for open source and I am always interested in working on new
               projects with new people. Feel free to reach out if you have anything to talk about,
@@ -161,7 +123,7 @@ export default function AuthorLayout({ children, frontMatter }) {
               </Link>{" "}
               or{" "}
               <Link
-                href={"https://api.whatsapp.com/send?phone=916358190818&text=hi"}
+                href={"https://api.whatsapp.com/send?phone=081219561519&text=hi"}
                 className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
               >
                 Whatsapp
@@ -180,71 +142,11 @@ export default function AuthorLayout({ children, frontMatter }) {
                 </svg>
               </Link>
             </p>
-            <br />
-            <p>
-              <Link
-                href={"/uses"}
-                className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
-              >
-                Here
-              </Link>{" "}
-              you can see what I use on daily basis
-            </p>
-            <br />
             <h1>About this site</h1>
             <p>
               Welcome to my home on the internet. This site functions as a blog/portfolio, a place
               to share code and thoughts. Opinions are my own.
             </p>
-            <div>
-              I learnt how to build this site from the most awesome people in the community:
-              <ul>
-                <li>
-                  <Link
-                    href={"https://github.com/timlrx/tailwind-nextjs-starter-blog"}
-                    className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
-                  >
-                    Timothy's Next.js and Tailwind CSS template
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      className="ml-0.5 inline-block h-4 w-4 fill-current"
-                    >
-                      <g data-name="Layer 2">
-                        <g data-name="external-link">
-                          <rect width="24" height="24" opacity="0" />
-                          <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
-                          <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
-                        </g>
-                      </g>
-                    </svg>
-                  </Link>
-                  : Template starter where I bootstrapped the project.
-                </li>
-                <li>
-                  <Link
-                    href={"https://www.einargudni.com/"}
-                    className="special-underline no-underline hover:text-gray-100 dark:text-gray-100 hover:dark:text-gray-100"
-                  >
-                    Einar Guðjónsson
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      className="ml-0.5 inline-block h-4 w-4 fill-current"
-                    >
-                      <g data-name="Layer 2">
-                        <g data-name="external-link">
-                          <rect width="24" height="24" opacity="0" />
-                          <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z" />
-                          <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z" />
-                        </g>
-                      </g>
-                    </svg>
-                  </Link>
-                  : Now page, navigation style, animations and much more.
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
         <div className="mt-10 md:pl-16">
@@ -262,9 +164,7 @@ export default function AuthorLayout({ children, frontMatter }) {
                 location={d.location}
                 range={d.range}
                 url={d.url}
-                text1={d.text1}
-                text2={d.text2}
-                text3={d.text3}
+                texts={d.texts}
               />
             ))}
           </div>
