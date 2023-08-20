c732298af574f1cd9e3d7c88faceab503d8fc34f
jamilmuhammad <mjamil1339@gmail.com>
2023-08-21 03:39:42 +0700

    update layout now, update fix hydrate

--- pages/now.js
+++ pages/now.js
@@ -1,12 +1,12 @@
-import Link from '@/components/Link'
-import { PageSEO } from '@/components/SEO'
-import { dayjs } from '@/components/DayJS'
-import { useEffect, useState } from 'react'
-import siteMetadata from '@/data/siteMetadata'
-import { getCurrentlyReading } from '@/lib/goodreads'
-import fetcher from 'lib/fetcher'
-import useSWR from 'swr'
-import { FaCloudShowersHeavy } from 'react-icons/fa'
+import Link from "@/components/Link"
+import { PageSEO } from "@/components/SEO"
+import { dayjs } from "@/components/DayJS"
+import { Suspense, useMemo, useState } from "react"
+import siteMetadata from "@/data/siteMetadata"
+import { getCurrentlyReading } from "@/lib/goodreads"
+import fetcher from "lib/fetcher"
+import useSWR from "swr"
+import { FaCloudShowersHeavy } from "react-icons/fa"
 import {
   BsCloudDrizzleFill,
   BsCloudsFill,
@@ -19,11 +19,13 @@ import {
   BsFillCloudSunFill,
   BsFillCloudMoonFill,
   BsFillCloudFill,
-} from 'react-icons/bs'
+} from "react-icons/bs"
+import SectionContainer from "@/components/SectionContainer"
+import Image from "@/components/Image"
 
 export const getServerSideProps = async () => {
   const response = await fetch(
-    'https://api.openweathermap.org/data/2.5/weather?lat=23.014770&lon=72.526330&appid=1b3c10c18e894eaf1fd63eedde53fa54&units=metric'
+    `https://api.openweathermap.org/data/2.5/weather?lat=-6.2297209&lon=106.664705&appid=926356e81e0f4ed0f5ba6b978ce4523e&units=metric`
   )
   const data = await response.json()
 
@@ -35,9 +37,9 @@ export const getServerSideProps = async () => {
 }
 
 export default function Now(currentlyReading) {
-  const { data } = useSWR('/api/now-playing', fetcher)
-  let currentlyReadingData = currentlyReading['currentlyReading']
-  let weatherData = currentlyReading['data']
+  const { data } = useSWR("/api/now-playing", fetcher)
+  let currentlyReadingData = currentlyReading["currentlyReading"]
+  let weatherData = currentlyReading["data"]
   const { temp: temperature } = weatherData.main
   const { icon: weatherIcon, description: weatherDescription } = weatherData.weather[0]
 
@@ -65,19 +67,14 @@ export default function Now(currentlyReading) {
   var year = new Date().getFullYear()
   var month = new Date().getMonth()
   var date = new Date().getDate()
-  var hour = new Date().getHours()
-  var minute = new Date().getMinutes()
-  var second = new Date().getSeconds()
-  const now = () => dayjs().tz()
-  const format = 'hhA'
-  const [TodayDate, setDate] = useState(now())
+  const [TodayDate, setDate] = useState(() => dayjs().tz("Asia/Jakarta"))
 
-  useEffect(() => {
-    const timer = setInterval(() => setDate(now()), 1000)
+  useMemo(() => {
+    const timer = setInterval(() => setDate(() => dayjs().tz("Asia/Jakarta")), 1000)
     return () => clearInterval(timer)
-  }, [])
+  }, [TodayDate])
 
-  var JamilBirthDate = '2000-04-16'
+  var JamilBirthDate = "2000-04-16"
   var birthDate = new Date(JamilBirthDate)
 
   var JamilAge = year - birthDate.getFullYear()
@@ -107,24 +104,24 @@ export default function Now(currentlyReading) {
     days: JamilDay,
   }
 
-  var ageString = ''
+  var ageString = ""
   if (age.years > 0 && age.months > 0 && age.days > 0)
-    ageString = age.years + ' years, ' + age.months + ' months, and ' + age.days + ' days old'
+    ageString = age.years + " years, " + age.months + " months, and " + age.days + " days old"
   else if (age.years == 0 && age.months == 0 && age.days > 0)
-    ageString = 'Only ' + age.days + ' days old'
+    ageString = "Only " + age.days + " days old"
   else if (age.years > 0 && age.months == 0 && age.days == 0)
-    ageString = age.years + ' years old. Happy Birthday!!'
+    ageString = age.years + " years old. Happy Birthday!!"
   else if (age.years > 0 && age.months > 0 && age.days == 0)
-    ageString = age.years + ' years and ' + age.months + ' months old'
+    ageString = age.years + " years and " + age.months + " months old"
   else if (age.years == 0 && age.months > 0 && age.days > 0)
-    ageString = age.months + ' months and ' + age.days + ' days old'
+    ageString = age.months + " months and " + age.days + " days old"
   else if (age.years > 0 && age.months == 0 && age.days > 0)
-    ageString = age.years + ' years, and' + age.days + ' days old'
-  else if (age.years == 0 && age.months > 0 && age.days == 0) ageString = age.months + ' months old'
+    ageString = age.years + " years, and" + age.days + " days old"
+  else if (age.years == 0 && age.months > 0 && age.days == 0) ageString = age.months + " months old"
   else ageString = "Welcome to Earth! <br> It's first day on Earth!"
 
   return (
-    <>
+    <SectionContainer>
       <PageSEO
         title={`Now - ${siteMetadata.author}`}
         description="What I'm working on now"
@@ -133,17 +130,14 @@ export default function Now(currentlyReading) {
       <div>
         <div className="my-2">
           <h3>Where am I and what am I doing?</h3>
-          <div className=" mt-4 mb-6 text-xs text-neutral-700 dark:text-neutral-400">
-            This page was automatically updated @ {date}-{month}-{year} {hour}:{minute}:{second}
-          </div>
         </div>
         {/* Misc */}
         <div>
           <div className="flex justify-between gap-5">
-            <div className="mt-2 mb-10 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
-              <span className="ml-2 font-semibold">Location:</span> <span>Ahmedabad, India</span>
+            <div className="mb-10 mt-2 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
+              <span className="ml-2 font-semibold">Location:</span> <span>Jakarta, Indonesia</span>
               <br />
-              <span className="ml-2 font-semibold">Weather:</span>{' '}
+              <span className="ml-2 font-semibold">Weather:</span>{" "}
               <span>
                 <a
                   href="https://weather.com/en-GB/weather/today/l/f42d9f8baa19b4d8d5e034449faa703839993366f64551a56a2b530297075dc2"
@@ -151,22 +145,23 @@ export default function Now(currentlyReading) {
                   rel="noopener noreferrer"
                   className="underline-offset-1 hover:underline"
                 >
-                  {icons[`_${weatherIcon}`]} Currently <b>{parseInt(temperature)}°C</b>
-                  {' with '}
+                  <Suspense>{icons[`_${weatherIcon}`]}</Suspense> Currently{" "}
+                  <b>{parseInt(temperature)}°C</b>
+                  {" with "}
                   <span>{weatherDescription}</span>
                 </a>
               </span>
             </div>
 
-            <div className="mt-2 mb-10 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
-              <span className="ml-2 font-semibold">Reading:</span>{' '}
+            <div className="mb-10 mt-2 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
+              <span className="ml-2 font-semibold">Reading:</span>{" "}
               <a
                 href={currentlyReadingData[0].url}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="underline-offset-1 hover:underline"
               >
-                <span>{currentlyReadingData[0].title}</span> by{' '}
+                <span>{currentlyReadingData[0].title}</span> by{" "}
                 <span>{currentlyReadingData[0].author}</span>
               </a>
               <br />
@@ -175,19 +170,21 @@ export default function Now(currentlyReading) {
           </div>
 
           <div className="-my-6 flex justify-between gap-5">
-            <div className="mt-2 mb-10 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
-              <span className="ml-2 font-semibold">Date:</span>{' '}
-              <span>{TodayDate.format('DD/MM/YYYY')}</span>
+            <div className="mb-10 mt-2 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
+              <span className="ml-2 font-semibold">Date:</span>{" "}
+              <span>{TodayDate.format("DD/MM/YYYY")}</span>
               <br />
-              <span className="ml-2 font-semibold">Time:</span>{' '}
+              <span className="ml-2 font-semibold">Time:</span>{" "}
               <span>
-                <BsClock className="mb-0.5 inline h-3 w-3 hover:animate-spin" />{' '}
-                {TodayDate.format('h:mm:ss A')}
+                <Suspense>
+                  <BsClock className="mb-0.5 inline h-3 w-3 hover:animate-spin" />{" "}
+                </Suspense>
+                {TodayDate.format("h:mm:ss A")}
               </span>
             </div>
 
-            <div className="mt-2 mb-10 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
-              <span className="ml-2 font-semibold">Listening:</span>{' '}
+            <div className="mb-10 mt-2 w-1/2 rounded-md border border-gray-600 p-1 text-sm dark:border-gray-200">
+              <span className="ml-2 font-semibold">Listening:</span>{" "}
               <span>
                 {data?.songUrl ? (
                   <a
@@ -196,7 +193,18 @@ export default function Now(currentlyReading) {
                     rel="noopener noreferrer"
                     className="underline-offset-1 hover:underline"
                   >
-                    <span>{data.title}</span>
+                    <Image
+                      src={data.albumImageUrl}
+                      width={70}
+                      height={70}
+                      alt={`song image ${data.title}, ${data.artist}`}
+                      className="h-25 w-25 rounded-md dark:shadow-gray-800"
+                      placeholder="blur"
+                      blurDataURL="/static/images/SVG-placeholder.png"
+                    />
+                    <span>
+                      {data.title}, {data.artist}
+                    </span>
                   </a>
                 ) : (
                   <span>Not Playing</span>
@@ -210,86 +218,7 @@ export default function Now(currentlyReading) {
         <div className="justify-center text-center text-2xl font-medium text-gray-200 dark:text-gray-600">
           &#126;&#126;&#126;
         </div>
-        {/* Work */}
-        <div className="pb-4">
-          <p>
-            I work as a Data Engineer at{' '}
-            <Link
-              href={'https://www.accenture.com/'}
-              className="special-underline no-underline dark:text-gray-100 hover:dark:text-gray-100"
-            >
-              Accenture
-            </Link>
-            .
-          </p>
-          <br />
-          <p>
-            I work on building pipelines and automating the entire process using Scala and Gcloud.
-          </p>
-          <br />
-          <p>
-            I have been trying to shift my field from Data Engineering to Data Science. I have been
-            constantly applying for the same as well.
-          </p>
-          <br />
-          <p>
-            My location preference is Bangalore but I am open to shift to another place for a better
-            opportunity.
-          </p>
-          <br />
-          <p>
-            I'm always trying to learn more, and at the moment I'm trying to follow this{' '}
-            <Link
-              href={
-                'https://www.youtube.com/watch?v=_u-PaJCpwiU&list=PLu0W_9lII9ai6fAMHp-acBmJONT7Y4BSG'
-              }
-              className="special-underline no-underline dark:text-gray-100 hover:dark:text-gray-100"
-            >
-              Machine Learning tutorial
-            </Link>
-            .
-          </p>
-        </div>
-        <div className="justify-center text-center text-2xl font-medium text-gray-200 dark:text-gray-600">
-          &#126;&#126;&#126;
-        </div>
-
-        {/* Personal life */}
-        <div className="pt-6">
-          <p>
-            I've been slowly building this website, trying to share interesting things with anyone
-            who wants to read it.{' '}
-            <Link
-              href={'https://www.swyx.io/learn-in-public'}
-              className="special-underline no-underline dark:text-gray-100 hover:dark:text-gray-100"
-            >
-              This
-            </Link>{' '}
-            article is a great reason to start your blog.
-          </p>
-          <br />
-          <p>
-            I am also going to start applying for Masters in Data Science in August 2022 for intake
-            of Fall 2023. I hope to get a good university near the West Coast. I'll update this page
-            after I get my admits.
-          </p>
-          <br />
-          <p>
-            I recently started to draft a post about my new Obsidian worklfow, it will be a good
-            one!
-          </p>
-        </div>
-        <div className="mt-3 text-sm">
-          For more examples of folks with /now pages, check out{' '}
-          <Link
-            href={'https://nownownow.com/'}
-            className="special-underline no-underline dark:text-gray-100 hover:dark:text-gray-100"
-          >
-            nownownow.com
-          </Link>
-          .
-        </div>
       </div>
-    </>
+    </SectionContainer>
   )
 }
