fcc525a40ad002ac80c0d960ae2209239f1c0442
jamilmuhammad <mjamil1339@gmail.com>
2023-10-05 00:47:56 +0700

    update migrate source from call api

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -2,54 +2,55 @@ import { TagSEO } from "@/components/SEO"
 import siteMetadata from "@/data/siteMetadata"
 import ListLayout from "@/layouts/ListLayout"
 import generateRss from "@/lib/generate-rss"
-import { getAllFilesFrontMatter } from "@/lib/mdx"
-import { getAllTags } from "@/lib/tags"
-import kebabCase from "@/lib/utils/kebabCase"
+import { ARTICLE_URL, TAG_URL, TAG_URL_PATH } from "@/lib/utils/constants"
+import { fetchData } from "@/service/article"
 import fs from "fs"
 import path from "path"
 
 const root = process.cwd()
 
 export async function getStaticPaths() {
-  const tags = await getAllTags("blog")
+  const { data: paths } = await fetchData(TAG_URL_PATH)
 
   return {
-    paths: Object.keys(tags).map((tag) => ({
+    paths: paths.map((p) => ({
       params: {
-        tag,
+        tag: p.path,
       },
     })),
     fallback: false,
   }
 }
 
-export async function getStaticProps({ params }) {
-  const allPosts = await getAllFilesFrontMatter("blog")
-  const filteredPosts = allPosts.filter(
-    (post) => post.draft !== true && post.tags.map((t) => kebabCase(t)).includes(params.tag)
-  )
+export async function getStaticProps({ params: { tag } }) {
+  const { data, pagination, meta, status } = await fetchData(ARTICLE_URL("", tag))
 
-  // rss
-  if (filteredPosts.length > 0) {
-    const rss = generateRss(filteredPosts, `tags/${params.tag}/feed.xml`)
-    const rssPath = path.join(root, "public", "tags", params.tag)
+  // //   // rss
+  if (data.length > 0) {
+    const rss = generateRss(data, `tags/${tag}/feed.xml`)
+    const rssPath = path.join(root, "public", "tags", tag)
     fs.mkdirSync(rssPath, { recursive: true })
     fs.writeFileSync(path.join(rssPath, "feed.xml"), rss)
   }
 
-  return { props: { posts: filteredPosts, tag: params.tag } }
+  return {
+    props: {
+      data,
+      pagination,
+      tag,
+    },
+    revalidate: 43200,
+  }
 }
 
-export default function Tag({ posts, tag }) {
-  // Capitalize first letter and convert space to dash
-  const title = tag[0].toUpperCase() + tag.split(" ").join("-").slice(1)
+export default function Tag({ data, pagination, tag }) {
   return (
     <>
       <TagSEO
         title={`${tag} - ${siteMetadata.author}`}
         description={`${tag} tags - ${siteMetadata.author}`}
       />
-      <ListLayout posts={posts} title={title} />
+      <ListLayout posts={data} pagination={pagination} title={tag} />
     </>
   )
 }
