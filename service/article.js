b1a56a1c81933d1635ab214ccfcf5a86a7444326
jamilmuhammad <mjamil1339@gmail.com>
2023-10-05 21:59:50 +0700

    update header fetch

--- service/article.js
+++ service/article.js
@@ -8,7 +8,7 @@ const ARTICLE_DATA = []
 export const fetchData = async (url) => {
   const response = await fetch(url, {
     headers: {
-      "Content-Type": "application/json",
+      Accept: "*/*",
     },
     method: "GET",
   })
