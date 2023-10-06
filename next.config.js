f3703480f4a5df1fb82485977456379595369597
jamilmuhammad <mjamil1339@gmail.com>
2023-10-06 22:49:41 +0700

    add local analytics umami

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is analytics-jamilmuhammadblog.vercel.app;
   style-src 'self' 'unsafe-inline'
   font-src 'self'
   img-src * blob: data:;
