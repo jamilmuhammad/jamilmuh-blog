b057d9533b2a0a2502df2f4c6c350daf3bc22b5f
jamilmuhammad <mjamil1339@gmail.com>
2023-10-09 20:21:02 +0700

    add event analytics umami

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,9 +2,9 @@ const headerNavLinks = [
   { href: "/", title: "Home" },
   { href: "/blog", title: "Blog" },
   { href: "/tags", title: "Tags" },
-  { href: "/projects", title: "Projects" },
-  { href: "/guestbook", title: "Guestbook" },
-  { href: "/about", title: "About" },
+  { href: "/projects", title: "Projects", eventName: "Navigate Menu: Projects" },
+  { href: "/guestbook", title: "Guestbook", eventName: "Navigate Menu: Guestbook" },
+  { href: "/about", title: "About", eventName: "Navigate Menu: About" },
 ]
 
 export default headerNavLinks
