b48fe17dc1bace80a0f9a91f11da480435ef610e
jamilmuhammad <mjamil1339@gmail.com>
2023-10-09 20:18:56 +0700

    add event analytics umami

--- pages/contact.js
+++ pages/contact.js
@@ -10,27 +10,31 @@ const Contact = () => {
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
           Contact
         </h1>
-        <div className="pt-10 pb-8">
+        <div className="pb-8 pt-10">
           <ul className="font-semi-bold flex flex-col space-y-4">
             <ContactLink
               href="mailto:jamielmuhammadd@gmail.com"
               title="gmail"
               icon="jamielmuhammadd"
+              data-umami-event="Contact Social: Email"
             />
             <ContactLink
               href="https://github.com/jamilmuhammad"
               title="github"
               icon="jamilmuhammad"
+              data-umami-event="Contact Social: Github"
             />
             <ContactLink
               href="https://twitter.com/jamilmuhammaddd"
               title="twitter"
               icon="jamilmuhammaddd"
+              data-umami-event="Contact Social: Twitter"
             />
             <ContactLink
               href="https://www.linkedin.com/in/jamilmuhammad/"
               title="linkedin"
               icon="jamilmuhammad"
+              data-umami-event="Contact Social: Linkedin"
             />
           </ul>
         </div>
